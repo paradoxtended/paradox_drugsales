31706650e12be90f4d3a0c75108cd37bcbf3cd04
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 13:37:14 +0000

    chore: bump manifest version to v0.4.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 game 'gta5'
 name 'prp-drugsales'
 author 'Paradoxtended'
-version '0.3.0'
+version '0.4.0'
 repository 'https://github.com/paradoxtended/prp-drugsales'
 description 'Advanced drug selling for FiveM'
 
