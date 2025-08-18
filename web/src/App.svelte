37ca2a089c9cdb92f1a8d57835074c8a8936f3e0
Timothy Barabas <215121906+theravaget@users.noreply.github.com>
2025-08-18 14:53:26 +0200

    feat(ui): retype of leaderboard

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -15,6 +15,20 @@ debugData([
     data: {
       users: [
         {
+          name: 'Esteban Casados',
+          nickname: 'Esteee',
+          imageUrl: 'https://i.postimg.cc/BQ08mBGj/Sunset-in-Grace-Bay-Turks-and-Caicos-Islands-scaled.jpg',
+          stats: {
+            earned: 184752,
+            lastActive: '08/14/2025, 02:27:48 PM',
+            reputation: 12.4516
+          },
+          drugs: {
+            weed_joint: { label: 'Weed Joint', amount: 41 },
+            weed_bag: { label: 'Weed Bag', amount: 94 }
+          }
+        },
+        {
           name: 'Enzo Favara',
           nickname: 'Enzoo',
           imageUrl: 'https://i.postimg.cc/8CCKvsRZ/Enzo-Favara.jpg',
@@ -44,7 +58,55 @@ debugData([
             weed_joint: { label: 'Weed Joint', amount: 41 },
             weed_bag: { label: 'Weed Bag', amount: 94 }
           }
-        }
+        },
+        {
+          name: 'Enzo Favara',
+          nickname: 'Enzoo',
+          imageUrl: 'https://i.postimg.cc/8CCKvsRZ/Enzo-Favara.jpg',
+          stats: {
+            earned: 688421,
+            lastActive: '08/16/2025, 03:27:45 PM',
+            reputation: 24.564
+          },
+          drugs: {
+            meth_bag: { label: 'Meth Bag', amount: 48 },
+            coke_bag: { label: 'Coke Bag', amount: 75 },
+            meth_syringe: { label: 'Meth Syringe', amount: 92 },
+            coke_brick: { label: 'Coke Brick', amount: 150 }
+          },
+          myself: true
+        },
+        {
+          name: 'Esteban Casados',
+          nickname: 'Esteee',
+          imageUrl: 'https://i.postimg.cc/BQ08mBGj/Sunset-in-Grace-Bay-Turks-and-Caicos-Islands-scaled.jpg',
+          stats: {
+            earned: 184752,
+            lastActive: '08/14/2025, 02:27:48 PM',
+            reputation: 12.4516
+          },
+          drugs: {
+            weed_joint: { label: 'Weed Joint', amount: 41 },
+            weed_bag: { label: 'Weed Bag', amount: 94 }
+          }
+        },
+        {
+          name: 'Enzo Favara',
+          nickname: 'Enzoo',
+          imageUrl: 'https://i.postimg.cc/8CCKvsRZ/Enzo-Favara.jpg',
+          stats: {
+            earned: 688421,
+            lastActive: '08/16/2025, 03:27:45 PM',
+            reputation: 24.564
+          },
+          drugs: {
+            meth_bag: { label: 'Meth Bag', amount: 48 },
+            coke_bag: { label: 'Coke Bag', amount: 75 },
+            meth_syringe: { label: 'Meth Syringe', amount: 92 },
+            coke_brick: { label: 'Coke Brick', amount: 150 }
+          },
+          myself: true
+        },
       ],
       admin: true
     }
--- web/src/lib/components/Leaderboard.svelte
+++ web/src/lib/components/Leaderboard.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 import { dataState, NuiState, nuiState } from "$lib/utils";
 import { fetchNui } from "$lib/utils/fetchNui";
-import { linear } from "svelte/easing";
-import { fade, slide } from "svelte/transition";
+import { fade } from "svelte/transition";
 
 interface User {
     identifier: string;
@@ -18,261 +17,122 @@ interface User {
     myself?: boolean;
 }
 
+interface ExtendedUser extends User {
+    id: number;
+}
+
 interface LeaderboardProps {
     users: User[];
     admin?: boolean;
 }
 
 let data: LeaderboardProps = $state($dataState as unknown as LeaderboardProps);
-let activeTab: 'PROFILE' | 'LEADERBOARD' = $state('LEADERBOARD');
-let users: User[] = $state([]);
-let query: string = $state('');
-let player: User = data.users.find(user => user?.myself) ?? data.users[0];
-let currentProfile: User | undefined = $state();
-let currentSetting: 'nickname' | 'imageUrl' | null = $state(null);
+let pageSize: number = 5;
 
-let categoriesEl: HTMLElement | undefined = $state();
-let bgSliderEl: HTMLElement | undefined = $state();
-let sliderEl: HTMLElement | undefined = $state();
-
-function updateSlider() {
-    if (!categoriesEl || !bgSliderEl || !sliderEl) return;
-
-    const pEls = categoriesEl.querySelectorAll('p');
-    const activeEl = Array.from(pEls).find(el => el.classList.contains('active')) as HTMLElement;
-    if (!activeEl) return;
-
-    const rect = activeEl.getBoundingClientRect();
-    const containerRect = categoriesEl.getBoundingClientRect();
-
-    const width = rect.width;
-    const offsetLeft = rect.left - containerRect.left;
-
-    bgSliderEl.style.width = width + 'px';
-    bgSliderEl.style.transform = `translateX(${offsetLeft}px)`;
-
-    sliderEl.style.width = width + 'px';
-    sliderEl.style.transform = `translateX(${offsetLeft}px)`;
-}
+let time: string = $state('08:42 AM');
+let TAB: 'home' | 'profile' = $state('home');
+let searchQuery: string = $state('');
+let Users: ExtendedUser[] = $state([]);
+let Player: User = data.users.find(user => user.myself) || data.users[0];
+let SortType: 'ASC' | 'DESC' = $state('DESC');
+let PAGE: number = $state(1);
+let totalPages = $state(Math.max(1, Math.ceil(Users.length / pageSize)));
 
 $effect(() => {
-    if (activeTab) {
-        updateSlider();
-    }
+    const sorted = [...data.users].sort((a, b) => SortType === 'DESC' ? b.stats.earned - a.stats.earned : a.stats.earned - b.stats.earned);
 
-    users = data.users
-        .filter(user =>
-            user.nickname.toLowerCase().includes(query.toLowerCase()) ||
-            user.name.toLowerCase().includes(query.toLowerCase()) && data.admin)
-        .sort((a, b) => b.stats.earned - a.stats.earned)
-});
+    // Import id to user
+    const ranked = sorted.map((user, id) => ({ ...user, id: id + 1 }));
 
-function getDrugs(drugs: Record<string, { label: string, amount: number }>) {
-    const allDrugs = Object.values(drugs).filter(drug => drug.amount > 0);
+    const filter = ranked.filter(user => 
+        user.nickname.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        (data.admin && user.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    )
+
+    totalPages = Math.max(1, Math.ceil(filter.length / pageSize));
+
+    Users = filter.slice((PAGE - 1) * pageSize, PAGE * pageSize);
+})
+
+function getPlayerDrugs(user?: User) {
+    const player = user || Player;
+    const allDrugs = Object.values(player.drugs).filter(drug => drug.amount > 0);
     const sorted = allDrugs.sort((a, b) => b.amount - a.amount);
 
-    return sorted.length > 0 ? [
-        sorted.slice(0, 3).map(drug => drug.label).join(', '), 
-        sorted.map(drug => `${drug.label}: ${drug.amount}`).join(', ')
-    ] : [
-        'No drugs sold',
-        'No drugs sold'
-    ]
-}
-
-function openProfile(user?: User) {
-    const profile = user || player;
-    
-    if (profile === null) return;
-
-    currentSetting = null;
-    activeTab = 'PROFILE';
-    currentProfile = profile;
-}
-
-async function editProfile() {
-    const input = (document.getElementById('setting-input') as HTMLInputElement).value;
-    
-    if (input === null || input === '' || currentSetting === null || currentProfile === undefined) return;
-
-    const response = await fetchNui('editProfile', { identifier: currentProfile.identifier, type: currentSetting, input: input });
-
-    if (response) {
-        data.users = data.users.map(user => {
-            if (user.identifier === currentProfile!.identifier) {
-                return {
-                    ...user,
-                    [currentSetting!]: input
-                }
-            }
-
-            return user
-        });
-
-        player[currentSetting!] = input;
-
-        currentProfile = {
-            ...currentProfile,
-            [currentSetting]: input
-        }
-    }
-
-    currentSetting = null;
+    // return first 3 drugs
+    return sorted.slice(0, 3).map(drug => drug.label).join(', ')
 }
 
 </script>
 
 {#if $nuiState === NuiState.Leaderboard}
-    <div class="lb-container" transition:fade|global={{easing: linear}}>
-        <!-- Main tab (header) -->
-        <div class="flex items-center justify-between">
-            <div class="lb-title-container">
-                <p class="text-[#2dd4bf] font-medium text-xl drop-shadow-[0_0_10px_#2dd4bf] leading-6">Drug Dealers</p>
-                <p class="text-sm text-gray-400">Find dealers</p>
+    <div class="lb-container" transition:fade|global>
+        <div class="lb-header">
+            <div>
+                <p>{time}</p>
+                <i class="fa-solid fa-house"></i>
             </div>
-            <div class="flex items-center relative" bind:this={categoriesEl}>
-                <p class="lb-category" class:active={activeTab === 'LEADERBOARD'} onclick={() => activeTab = 'LEADERBOARD'}>LEADERBOARD</p>
-                <p class="lb-category" class:active={activeTab === 'PROFILE'} onclick={() => openProfile()}>PROFILE</p>
-                <div class="lb-category-bg-slider" bind:this={bgSliderEl}></div>
-                <div class="lb-category-slider" bind:this={sliderEl}></div>
-            </div>
-            <div class="relative">
-                <input type="text" placeholder="Search" class="lb-input-search" bind:value={query}>
-                <i class="hgi hgi-stroke hgi-search-01 text-[#adadad] absolute top-1/2 -translate-y-1/2 right-3 text-sm pointer-events-none"></i>
-            </div>
-            <div class="flex items-center text-[13px] text-gray-400 bg-gray-500/30 p-[1px]">
-                <p class="bg-gray-800/50 px-2 py-0.5">Exit</p>
-                <p class="px-2 py-0.5">ESC</p>
+            <div>
+                <i class="fa-solid fa-user"></i>
             </div>
         </div>
 
-        <div class="lb-divider after:-bottom-[9px] z-0"></div>
-
-        <!-- Leaderboard -->
-        {#if activeTab === 'LEADERBOARD'}
-            <div in:fade={{ duration: 300, easing: linear }}
-            class="mt-5 overflow-auto h-[400px] -mr-[15px] pr-[15px]">
-                <table class="w-full border-separate" style="border-spacing: 0 10px;">
-                    <thead>
-                        <tr class="text-[#9ca3af] text-sm bg-gradient-to-b from-neutral-500/0 to-[#4b556350]">
-                            <td class="text-center py-1.5">User</td>
-                            <td class="text-center py-1.5">Last active</td>
-                            <td class="text-center py-1.5">Most sellable drugs</td>
-                            <td class="text-center py-1.5">Earnings</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td colspan="4">
-                                <div class="lb-divider"></div>
-                            </td>
-                        </tr>
-                        {#each users as user}
-                            <tr class="text-sm text-white bg-[radial-gradient(#71717a75,_#374151a0)]">
-                                <td class="text-center p-2">
-                                    <div class="flex items-center gap-5">
-                                        <i class="fa-solid fa-user bg-gray-500/50 w-8 h-8 flex items-center justify-center rounded-sm duration-200
-                                        hover:bg-[#2dd4bf20] hover:text-[#2dd4bf] cursor-pointer"
-                                        onclick={() => openProfile(user)}></i>
-                                        <p>{user.nickname}</p>
-                                    </div>
-                                </td>
-                                <td class="text-center p-2">{user.stats.lastActive}</td>
-                                <td class="text-center p-2">{getDrugs(user.drugs)[0]}</td>
-                                <td class="text-center p-2">{Intl.NumberFormat('en-US', { style: "currency", currency: 'USD', maximumFractionDigits: 0 }).format(user.stats.earned)}</td>
-                            </tr>
-                        {/each}
-                    </tbody>
-                </table>
-            </div>
-        {/if}
-
-        <!-- Profile -->
-        {#if activeTab === 'PROFILE' && currentProfile !== undefined}
-            <div in:fade={{ duration: 300, easing: linear }} class="mt-7">
+        {#if TAB === 'home'}
+            <div class="px-5 py-3 relative">
                 <div class="flex items-center justify-between">
-                    <div class="flex items-center gap-3">
-                        <div class="relative w-[100px] h-[115px]">
-                            <!-- Hexagon border -->
-                            <div class="absolute inset-0 hexagon"></div>
-                            <!-- Image -->
-                            <img src={currentProfile.imageUrl} class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[92%] h-[92%]" style="clip-path: polygon(50% 0,100% 25%,100% 75%,50% 100%,0% 75%,0% 25%); aspect-ratio: cos(30deg);" />
-                        </div>
-                        <div>
-                            <p class="text-white text-2xl flex items-center gap-3 group">
-                                {currentProfile.nickname}
-                                <span class="text-gray-400 text-lg font-light duration-300 opacity-0 group-hover:opacity-100">{data.admin || currentProfile.myself ? currentProfile.name : ''}</span>
-                            </p>
-                            <p class="text-sm text-gray-400 font-light">Last active: {currentProfile.stats.lastActive}</p>
-                            <p class="text-sm text-gray-400 font-light">Reputation: {Number(currentProfile.stats.reputation.toFixed(2)).toString()}</p>
-                        </div>
+                    <div>
+                        <p class="text-[30px] leading-7">Leaderboard</p>
+                        <p class="text-gray-400">View the best sellers in this city</p>
                     </div>
-                    {#if currentProfile.myself || data.admin}
-                        <div class="flex flex-col gap-2">
-                            <button class="text-sm text-[#2dd4bf] font-light bg-[#2dd4bf20] py-[5px] px-[17px] duration-300 hover:bg-[#2dd4bf30] rounded-sm"
-                            onclick={() => currentSetting = 'nickname'}>Change username</button>
-                            <button class="text-sm text-[#2dd4bf] font-light bg-[#2dd4bf20] py-[5px] px-[17px] duration-300 hover:bg-[#2dd4bf30] rounded-sm"
-                            onclick={() => currentSetting = 'imageUrl'}>Change profile picture</button>
-                        </div>
-                    {/if}
+
+                    <div class="relative">
+                        <input type="text" placeholder="Search..." class="bg-neutral-900 rounded px-2 pr-8 py-1 text-sm font-[Inter] placeholder:text-neutral-500 w-[200px]
+                        focus:outline focus:outline-1 focus:outline-offset-1 focus:outline-lime-500" bind:value={searchQuery}>
+                        <i class="hgi hgi-stroke hgi-search-01 text-neutral-500 pointer-events-none absolute top-1/2 -translate-y-1/2 right-2"></i>
+                    </div>
                 </div>
 
-                <div class="lb-divider pt-3"></div>
+                <div class="mt-3">
+                    <table class="lb-table">
+                        <thead>
+                            <tr>
+                                <td>Rank</td>
+                                <td>User</td>
+                                <td>Last active</td>
+                                <td>Most sellable drugs</td>
+                                <td>Earnings</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {#if Users.length < 1}
+                                <tr class="empty">
+                                    <td colspan="5" class="text-4xl">No users</td>
+                                </tr>
+                            {/if}
 
-                <div class="overflow-auto h-[260px] -mr-[15px] pr-[15px] mt-3">
-                    {#if currentSetting}
-                        {#key currentSetting}
-                            <div class="bg-[radial-gradient(#71717a75,_#374151a0)] p-3 mb-3 rounded-sm flex items-center justify-between" 
-                                in:slide|global>
-                                <div>
-                                    <p class="text-[#2dd4bf] font-medium text-lg drop-shadow-[0_0_10px_#2dd4bf] leading-6">Change {currentSetting === 'imageUrl' ? 'profile picture' : 'nickname'}</p>
-                                    <p class="text-sm text-gray-400">Change your {currentSetting === 'imageUrl' ? 'profile picture' : 'nickname'} to your likings.</p>
-                                </div>
-                                <div class="flex items-center gap-3">
-                                    <input id="setting-input" type="text" placeholder={currentSetting === 'imageUrl' ? 'New profile picture' : 'New nickname'} class="lb-input-search" defaultValue={currentSetting === 'imageUrl' ? currentProfile.imageUrl : currentProfile.nickname}>
-                                    <div class="relative w-[30px] h-[35px]">
-                                        <!-- Hexagon border -->
-                                        <div class="absolute inset-0 hexagon"></div>
-                                        <div class="bg-[#2dd4bf20] w-full h-full cursor-pointer duration-300 hover:bg-[#2dd4bf30]"
-                                        style="clip-path: polygon(50% 0,100% 25%,100% 75%,50% 100%,0% 75%,0% 25%); aspect-ratio: cos(30deg);"
-                                        onclick={() => editProfile()}>
-                                            <i class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fa-solid fa-check text-[#2dd4bf]"></i>
+                            {#each Users as user}
+                                <tr>
+                                    <td>
+                                        <p><span class="text-[10px] text-gray-400">#</span>{user.id}</p>
+                                    </td>
+                                    <td>
+                                        <div class="flex items-center justify-center gap-3">
+                                            <img src={user.imageUrl} class="w-[35px] h-[35px] rounded-full">
+                                            <p>{user.nickname}</p>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                        {/key}
-                    {/if}
+                                    </td>
+                                    <td>{user.stats.lastActive}</td>
+                                    <td>{getPlayerDrugs(user)}</td>
+                                    <td>{Intl.NumberFormat('en-US', { style: "currency", currency: 'USD', maximumFractionDigits: 0 }).format(user.stats.earned)}</td>
+                                </tr>
+                            {/each}
+                        </tbody>
+                    </table>
+                </div>
 
-                    <div class="bg-[radial-gradient(#71717a75,_#374151a0)] p-3 rounded-sm">
-                        <div>
-                            <p class="text-[#2dd4bf] font-medium text-lg drop-shadow-[0_0_10px_#2dd4bf] leading-6">Statistics</p>
-                            <p class="text-sm text-gray-400">Check player's statistics.</p>
-                        </div>
-                        <div>
-                            <table class="w-full border-separate" style="border-spacing: 0 10px;">
-                                <thead>
-                                    <tr class="text-[#9ca3af] text-sm bg-gradient-to-b from-neutral-500/0 to-[#4b556350]">
-                                        <td class="text-center py-1.5">Reputation</td>
-                                        <td class="text-center py-1.5">Earnings</td>
-                                        <td class="text-center py-1.5">Sold drugs</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td colspan="4">
-                                            <div class="lb-divider"></div>
-                                        </td>
-                                    </tr>
-                                    <tr class="text-sm text-white bg-[radial-gradient(#71717a75,_#374151a0)]">
-                                        <td class="text-center p-2">{Number(currentProfile.stats.reputation.toFixed(2)).toString()}</td>
-                                        <td class="text-center p-2">{Intl.NumberFormat('en-US', { style: "currency", currency: 'USD', maximumFractionDigits: 0 }).format(currentProfile.stats.earned)}</td>
-                                        <td class="text-center p-2 max-w-[150px]">{getDrugs(currentProfile.drugs)[1]}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <div class="lb-pagination">
+                    <button onclick={() => PAGE--} disabled={PAGE <= 1} class="disabled:opacity-50">Previous</button>
+                    <button onclick={() => PAGE++} disabled={PAGE >= totalPages} class="disabled:opacity-50">Next</button>
                 </div>
             </div>
         {/if}
